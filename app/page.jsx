30e76635645a6caa1b1b2b053714e9f25094f334
Digitalknuckles <digitalknuckles@gmail.com>
2026-04-15 00:03:03 -0400

    Add useRef import to page.jsx

--- app/page.jsx
+++ app/page.jsx
@@ -1,6 +1,6 @@
 'use client';
 
-import { useState, useEffect } from 'react';
+import { useState, useEffect, useRef } from 'react';
 import Character from './components/Character';
 import Navigation from './components/Navigation';
 import NFTCard from './components/NFTCard';
