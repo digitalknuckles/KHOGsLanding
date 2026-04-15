14e71dd6f5957f47dbd258b6b3f38cb0d8c42a99
Digitalknuckles <digitalknuckles@gmail.com>
2026-04-15 01:33:37 -0400

    Add viewport meta tag for responsive design

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,6 +1,10 @@
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
+      <meta
+  name="viewport"
+  content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no"
+/>
       <body>{children}</body>
     </html>
   );
