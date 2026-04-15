c3359048a3e210f0e98544800b20e7a257bade77
Digitalknuckles <digitalknuckles@gmail.com>
2026-04-15 01:10:40 -0400

    Update NFTCard.jsx

--- app/components/NFTCard.jsx
+++ app/components/NFTCard.jsx
@@ -1,19 +1,33 @@
 'use client';
+import { useState } from 'react';
 
 export default function NFTCard({ nft }) {
+  const [open, setOpen] = useState(false);
+
+  if (!nft) {
+    return <div className="card show">No NFT Found</div>;
+  }
+
   return (
     <div className="card show">
-      <img src={nft.image} />
+      <img src={nft.image} className="nft-img" />
 
       <h3>{nft.name}</h3>
 
-      <div>
-        {nft.attributes?.map((a, i) => (
-          <div key={i}>
-            {a.trait_type}: {a.value}
-          </div>
-        ))}
-      </div>
+      <button className="dropdown" onClick={() => setOpen(!open)}>
+        {open ? 'Hide Traits ▲' : 'Show Traits ▼'}
+      </button>
+
+      {open && (
+        <div className="traits">
+          {nft.attributes?.map((a, i) => (
+            <div key={i} className="trait">
+              <span>{a.trait_type}</span>
+              <span>{a.value}</span>
+            </div>
+          ))}
+        </div>
+      )}
     </div>
   );
 }
