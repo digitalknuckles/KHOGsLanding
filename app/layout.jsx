39e2703bafc2ed6fefabe4948d35c6e63b1ca914
Digitalknuckles <digitalknuckles@gmail.com>
2026-04-15 22:13:10 -0400

    Update layout.jsx

--- app/layout.jsx
+++ app/layout.jsx
@@ -7,6 +7,7 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <head>
+        <title>KHOGs</title>
         <meta
           name="viewport"
           content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no"
