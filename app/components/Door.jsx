61c47d7413d43c1291431029fb4eef4123b4d9f4
Digitalknuckles <digitalknuckles@gmail.com>
2026-04-16 18:23:51 -0400

    Adjust doorY position calculation

--- app/components/Door.jsx
+++ app/components/Door.jsx
@@ -15,7 +15,7 @@ export default function Door({ onEnter, scale = 1 }) {
 
   // 🎯 POSITION (based on your background)
   const doorX = 1605 / 1920;
-  const doorY = 1015 / 1080;
+  const doorY = 1018 / 1080;
 
   return (
     <div
