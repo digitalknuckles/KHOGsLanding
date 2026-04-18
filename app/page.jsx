208db71c1b4b54dbb4c140a702e6da3a858c5f7f
Digitalknuckles <digitalknuckles@gmail.com>
2026-04-17 22:50:45 -0400

    Change z-index of door class from 3 to 2

--- app/page.jsx
+++ app/page.jsx
@@ -257,7 +257,7 @@ html, body {
 .door {
   position:absolute;
   transform-origin:bottom center;
-  z-index:3;
+  z-index:2;
   cursor:pointer;
   transition: transform 0.1s ease, filter 0.1s ease;
 }
