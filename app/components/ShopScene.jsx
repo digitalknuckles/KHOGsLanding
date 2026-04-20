32bd55d1844e1780462432615598c10e3f8198e8
Digitalknuckles <digitalknuckles@gmail.com>
2026-04-20 12:45:15 -0400

    Refactor ShopScene component with updated constants

--- app/components/ShopScene.jsx
+++ app/components/ShopScene.jsx
@@ -1,32 +1,37 @@
 'use client';
 
-import NPCManager from './NPCManager';
 import Character from './Character';
+import NPCManager from './NPCManager';
 
-const SHOP_BG = "https://ipfs.io/ipfs/bafybeiakrcczgynayzfosyklhq4vehyuugwieeujyfuebwhd6ipgf2o7fq";
-const COUNTER_1 = "https://ipfs.io/ipfs/bafybeiguouijuhj3k4ilyibnp324c3glfmsj6vumxy3lxduim6z3grllmm";
-const COUNTER_2 = "https://ipfs.io/ipfs/bafybeihwur3phaaphvzu3w57obpjaqwdhdnsfjwwyxc2hi5wet6rg6t5ia";
+const BG =
+  "https://ipfs.io/ipfs/bafybeiakrcczgynayzfosyklhq4vehyuugwieeujyfuebwhd6ipgf2o7fq";
+
+const COUNTER_BACK =
+  "https://ipfs.io/ipfs/bafybeiguouijuhj3k4ilyibnp324c3glfmsj6vumxy3lxduim6z3grllmm";
+
+const COUNTER_FRONT =
+  "https://ipfs.io/ipfs/bafybeihwur3phaaphvzu3w57obpjaqwdhdnsfjwwyxc2hi5wet6rg6t5ia";
 
 export default function ShopScene({ tab, tabsRef }) {
   return (
-    <div className="scene">
+    <div className="scene shop-scene">
 
-      {/* 🌄 LAYER 0 */}
-      <img className="layer bg" src={SHOP_BG} alt="bg" />
+      {/* 🖼️ LAYER 0 — BACKGROUND */}
+      <img className="layer bg" src={BG} alt="shop background" />
 
-      {/* 🧍 LAYER 1 (NPC BEHIND COUNTER) */}
-      <div className="layer npc-layer">
+      {/* 🧍 LAYER 1 — NPCs (BEHIND COUNTER) */}
+      <div className="npc-layer">
         <NPCManager />
       </div>
 
-      {/* 🪑 LAYER 2 */}
-      <img className="layer counter-back" src={COUNTER_1} alt="" />
+      {/* 🧱 LAYER 2 — COUNTER BACK */}
+      <img className="layer counter-back" src={COUNTER_BACK} alt="counter back" />
 
-      {/* 🪑 LAYER 3 */}
-      <img className="layer counter-front" src={COUNTER_2} alt="" />
+      {/* 🧱 LAYER 3 — COUNTER FRONT */}
+      <img className="layer counter-front" src={COUNTER_FRONT} alt="counter front" />
 
-      {/* 🎮 LAYER 4 */}
-      <Character currentTab={tab} tabsRef={tabsRef} />
+      {/* 🎮 LAYER 4 — CHARACTER */}
+      <Character currentTab={tab} tabsRef={tabsRef} scale={1.3} />
 
     </div>
   );
